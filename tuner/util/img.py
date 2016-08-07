f2967caf6540a14d946b2b25a63475dcc0fe415f
ahangchen <1418659400@qq.com>
2016-08-07 22:51:09 +0800

    绘制超参变化时损失变化曲线

--- tuner/util/img.py
+++ tuner/util/img.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 import matplotlib.pyplot as plt
+import re
 import seaborn
 
 from tuner.util import file_helper
@@ -35,26 +36,30 @@ def mark():
             cur_line.append(exp_data)
 
 
-def loss_line():
+def avg_line():
     exp_file = file_helper.read2mem('../../line')
     exp_datas = exp_file.split('\n')
-    cur_line = list()
-    line_cnt = 0
+    cur_sum = 0
+    cur_cnt = 0
+    avg_s = list()
+    is_loss = True
     for exp_data in exp_datas:
         if exp_data.startswith('=='):
-            plt.plot(cur_line)
-            line_cnt += 1
-            del cur_line[:]
+            if is_loss:
+                avg_s.append(cur_sum / cur_cnt)
+                cur_sum = 0
+                cur_cnt = 0
+            is_loss = not is_loss
+            # 两次一翻转
         elif exp_data.startswith('['):
-            # 超参数转图片名
-            jpg_name = str(
-                [num_str[0: 4].replace("-", "") for num_str in exp_data[1: -1].split(', ')]
-            )[1: -1].replace("'", "").replace(', ', '-')
-            plt.savefig(str(line_cnt / 2) + '-' + jpg_name + '.jpg')
-            plt.clf()
             continue
-        else:
-            cur_line.append(exp_data)
+        elif re.match("^\d+?\.\d+?$", exp_data) is not None:
+            if is_loss:
+                cur_sum += float(exp_data)
+                cur_cnt += 1
+    plt.plot(avg_s)
+    plt.show()
 
 if __name__ == '__main__':
     mark()
+    avg_line()

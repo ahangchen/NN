f276d0fb1dc03e91cde0766813e89b232f77ffbb
ahangchen <cweihang@foxmail.com>
2016-09-12 19:05:56 +0800

    test hp 2 trend done but not ideal

--- tuner/nn/hp2trend.py
+++ tuner/nn/hp2trend.py
@@ -1,10 +1,7 @@
 # coding=utf-8
-import random
-
+import os
 import numpy as np
 import tensorflow as tf
-import os
-import math
 
 
 def assign_diffable_vars2tensor(placeholder, input_size):
@@ -22,17 +19,23 @@ def assign_diffable_vars2tensor(placeholder, input_size):
     return tf_info_var_2d
 
 
-def tensor_reshape_with_matrix(input_matrix, input_shape, output_shape):
+def tensor_reshape_with_matrix(input_matrix, output_shape):
     # 一般从二维到一维
-    output_weight = tf.Variable(tf.truncated_normal([input_shape[1], output_shape[0]], mean=-0.1, stddev=0.1))
-    output_biase = tf.Variable(tf.truncated_normal([output_shape[0]]))
-    output = tf.nn.xw_plus_b(input_matrix, output_weight, output_biase)
-    return output
+    input_shape = input_matrix.get_shape()
+    left_weight = tf.Variable(tf.truncated_normal([output_shape[0], int(input_shape[0])], mean=-0.1, stddev=0.1))
+    left_biase = tf.Variable(tf.truncated_normal([int(input_shape[1])]))
+    left_output = tf.nn.xw_plus_b(left_weight, input_matrix, left_biase)
+    print(left_output)
+    right_weight = tf.Variable(tf.truncated_normal([int(input_shape[1]), output_shape[1]], mean=-0.1, stddev=0.1))
+    right_biase = tf.Variable(tf.truncated_normal([output_shape[1]]))
+    right_output = tf.nn.xw_plus_b(left_output, right_weight, right_biase)
+    print(right_output)
+    return right_output
 
 
 def dnn(input_tensor, input_shape, output_shape, drop_out=False, layer_cnt=2):
     # input shape 是一个二维数组
-    hidden_node_count = 1024
+    hidden_node_count = 64
     # start weight
     hidden_stddev = np.sqrt(2.0 / input_shape[1])
     weights1 = tf.Variable(
@@ -81,7 +84,9 @@ def dnn(input_tensor, input_shape, output_shape, drop_out=False, layer_cnt=2):
     return output
 
 
-def rnn(x, n_steps, n_input, n_hidden=128):
+def rnn(x, n_hidden=64):
+    n_input = int(x.get_shape()[0])
+    n_steps = int(x.get_shape()[1])
     x = tf.reshape(x, [-1, n_steps, n_input])  # (batch_size, n_steps, n_input)
     # # permute n_steps and batch_size
     x = tf.transpose(x, [1, 0, 2])
@@ -100,7 +105,7 @@ def rnn(x, n_steps, n_input, n_hidden=128):
 def grad_optimizer(var_list, loss):
     global_step = tf.Variable(0, trainable=False)
     learning_rate = tf.train.exponential_decay(
-        0.5, global_step, 50, 0.8, staircase=True)
+        1.5, global_step, 20, 0.8, staircase=True)
     optimizer = tf.train.GradientDescentOptimizer(learning_rate)
     gradients, return_v = zip(*optimizer.compute_gradients(loss, var_list=var_list))
     gradients, _ = tf.clip_by_global_norm(gradients, 1.25)
@@ -110,33 +115,36 @@ def grad_optimizer(var_list, loss):
 
 class FitTrendModel(object):
     def __init__(self, input_size, output_size):
-        rnn_step_cnt = 10
         self.graph = tf.Graph()
         self.hyper_cnt = input_size
+        self.save_path = "fit_trend.ckpt"
         with self.graph.as_default():
             # 接收输入
             self.ph_hypers = tf.placeholder(tf.float32, shape=[self.hyper_cnt], name='ph_hypers')
             self.tf_hypers = assign_diffable_vars2tensor(self.ph_hypers, self.hyper_cnt)
             # 先喂给一个神经网络产生input
             trend_input = dnn(self.tf_hypers, [self.hyper_cnt, 1], [output_size])
+            print(trend_input)
             # 通过一个RNN
-            trend_outputs = rnn(tf.concat(0, trend_input), rnn_step_cnt, output_size)
+            trend_outputs = rnn(trend_input, output_size)
+            print(tf.concat(0, trend_outputs))
             # RNN接一个小型NN
-            trend_output = tensor_reshape_with_matrix(tf.concat(0, trend_outputs), [output_size, rnn_step_cnt],
-                                                      [output_size])
+            trend_output = tensor_reshape_with_matrix(tf.concat(0, trend_outputs),
+                                                      [output_size, 1])
+            self.predict = trend_output
             # 实际的trend
-            self.train_label = [tf.placeholder(tf.float32, shape=[output_size])]
+            self.train_label = tf.placeholder(tf.float32, shape=[output_size], name='train_label')
             # 预测准确率
             predict_accuracy = tf.reduce_mean(tf.sqrt(tf.square(tf.sub(trend_output, tf.concat(0, self.train_label)))))
             # 稳定时损失
             stable_loss = trend_outputs[-1]
-            self.is_fit = tf.placeholder(tf.bool)
+            self.is_fit = tf.placeholder(tf.bool, name='is_fit')
             self.loss = tf.cond(self.is_fit, lambda: predict_accuracy, lambda: stable_loss)
 
             # 优化器
-            var_s = tf.trainable_variables()
-            self.v_hp_s = var_s[0: self.hyper_cnt - 1]
-            self.v_fit_s = [v for v in var_s if v not in self.v_hp_s]
+            self.var_s = tf.trainable_variables()
+            self.v_hp_s = self.var_s[0: self.hyper_cnt]
+            self.v_fit_s = [v for v in self.var_s if v not in self.v_hp_s]
 
             optimizer_fit, _ = grad_optimizer(self.v_fit_s, self.loss)
             optimizer_hp, self.grad_hps = grad_optimizer(self.v_hp_s, self.loss)
@@ -144,32 +152,39 @@ class FitTrendModel(object):
             self.optimizer = tf.cond(self.is_fit, lambda: optimizer_fit, lambda: optimizer_hp)
 
             self.saver = tf.train.Saver()
-        self.session = tf.Session(graph=self.graph)
 
-    def init(self):
-        with self.session:
-            tf.initialize_all_variables().run()
+    def init(self, init_hp, session):
+        if os.path.exists(self.save_path):
+            # Restore variables from disk.
+            self.saver.restore(session, self.save_path)
+        else:
+            init_feed = dict()
+            init_feed[self.ph_hypers] = init_hp
+            tf.initialize_all_variables().run(feed_dict=init_feed)
 
     def fit(self, input_data, trend):
         fit_dict = dict()
         fit_dict[self.is_fit] = True
         fit_dict[self.ph_hypers] = input_data
         fit_dict[self.train_label] = trend
-
-        with self.session as fit_session:
-            _, loss, hp_s = fit_session.run([self.optimizer, self.loss, self.grad_hps], feed_dict=fit_dict)
-        print(loss)
-        print(hp_s)
+        with tf.Session(graph=self.graph) as session:
+            self.init(input_data, session)
+            _, loss, predict = session.run([self.optimizer, self.loss, self.predict], feed_dict=fit_dict)
+            self.saver.save(session, self.save_path)
+            print(loss)
+            print(trend)
+            print(predict)
+            print('=' * 40)
 
 
 def init_model(input_size, output_size):
     model = FitTrendModel(input_size, output_size)
-    model.init()
     return model
 
 
 def fit_trend(model, input_data, trend):
     model.fit(input_data, trend)
+    print('fit ++')
 
 
 def train_hp():
--- tuner/nn/test_hp2trend.py
+++ tuner/nn/test_hp2trend.py
@@ -1,6 +1,27 @@
-def test_inputs():
-    return [[float(i) for i in range(j , 3 + j )] for j in range(100)]
+from tuner.nn.hp2trend import init_model, fit_trend
+import numpy as np
+import random
+
+
+def test_inputs(hp_cnt):
+    test_data = [[float(i) for i in range(j, hp_cnt + j)] for j in range(1, 101)]
+    random.shuffle(test_data)
+    return test_data
 
 
 def test_label(test_data):
-    return [hp[0]for hp in test_data]
+    return [[hp[0] / (hp[1] + x + hp[2]) * hp[3] / hp[4] for x in range(1, 10)] for hp in test_data]
+
+
+if __name__ == '__main__':
+    test_hps = test_inputs(5)
+    test_labels = test_label(test_hps)
+    print('test_data:')
+    print(len(test_hps))
+    print('test_label:')
+    print(len(test_labels))
+    model = init_model(len(test_hps[0]), len(test_labels[0]))
+    for test_hp, test_label in zip(test_hps, test_labels):
+        fit_trend(model, np.array(test_hp), np.array(test_label))
+
+
